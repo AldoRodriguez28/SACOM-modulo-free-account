c15823a92e18905c9843b5840ffc1761ac1a6721
Aldo Rodriguez <arodriguez.ronquillo@hotmail.com>
2026-07-16 10:38:25 -0600

    creando archjivos de enviroment para test

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,8 @@
 export const environment = {
   production: false,
-  googleMapsApiKey: 'TU_API_KEY_AQUI'
+  googleMapsApiKey: 'TU_API_KEY_AQUI',
+  API_URI: 'http://10.34.7.172:4300/api',
+  SHARED_MGMT_URI: 'https://test-servicios.adn.com.mx/sharedmanagement/api',
+  SHARED_MGMT_USER: 'SACOM-NEGOCIOS',
+  SHARED_MGMT_PASS: 'xui54n8w'
 };
